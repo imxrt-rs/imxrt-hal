655297b2438c3b52de55dbfec8bf3724a2a37abd
Tom Burdick <tom.burdick@electromatic.us>
2020-02-10 23:53:58 -0600

    Port of imxrt1062-hal ccm to imxrt-ral
    Notable is that a few things made this take a little while, a lack of
    write_with_zero() matching the svd2rust equivalent operation in stm32ral
    register macros, and the use of impl From<T> for Y to convert some
    hal types to ral values, instead some functions are likely to be better
    suited for that since ral does not use types where svd2rust does.

---
+++ imxrt-hal/src/iomuxc/gpio.rs
@@ -0,0 +1,23 @@
+//! Pad definitions
+//!
+//! The module exposes iMXRT1060 pads and alternatives. For each pad, we implement
+//! a series of methods `alt0()` through `alt9()`, depending on the capabilities
+//! of the pad.
+
+mod ad_b0;
+pub use ad_b0::*;
+
+mod ad_b1;
+pub use ad_b1::*;
+
+mod b0;
+pub use b0::*;
+
+mod b1;
+pub use b1::*;
+
+mod emc;
+pub use emc::*;
+
+mod sd_b0;
+pub use sd_b0::*;
---
+++ imxrt-hal/src/lib.rs
@@ -0,0 +1,78 @@
+//! NXP iMXRT hardware abstraction layer
+//!
+//! An [`embedded-hal`](https://crates.io/crates/embedded-hal) implementation
+//! targeting processors in NXP's IMXRT106x family.
+//!
+//! The HAL is a WIP. More documentation will become available once more capabilities
+//! are exposed.
+//!
+//! In some cases, the HAL simply re-exports peripherals from the peripheral access
+//! crates (PAC). If they are not re-exported, all PAC components are available
+//! in the `pac` module.
+//!
+//! To see examples of the HAL, check out the `teensy4-bsp` and the `teensy4-examples` crates.
+//! We will skip documentation example and tests, since we cannot yet test them as part
+//! of the `cargo test` workflow...
+
+#![no_std]
+
+pub use imxrt_ral as ral;
+
+pub mod ccm;
+//pub mod gpio;
+//pub mod i2c;
+//pub mod iomuxc;
+//pub mod pit;
+//pub mod pwm;
+//pub mod uart;
+
+pub mod dcdc {
+    use imxrt_ral as ral;
+    pub struct DCDC(pub(crate) ral::dcdc::Instance);
+    impl DCDC {
+        pub fn raw(&mut self) -> &ral::dcdc::Instance {
+            &self.0
+        }
+    }
+}
+
+pub struct Peripherals {
+    //pub iomuxc: iomuxc::IOMUXC,
+    //pub systick: ral::SYST,
+    pub ccm: ccm::CCM,
+    //pub pit: pit::UnclockedPIT,
+    pub dcdc: dcdc::DCDC,
+    //pub pwm1: pwm::UnclockedController<pwm::module::_1>,
+    //pub pwm2: pwm::UnclockedController<pwm::module::_2>,
+    //pub pwm3: pwm::UnclockedController<pwm::module::_3>,
+    //pub pwm4: pwm::UnclockedController<pwm::module::_4>,
+    //pub i2c: i2c::Unclocked,
+    //pub uart: uart::Unclocked,
+}
+
+impl Peripherals {
+    pub fn take() -> Option<Self> {
+        let p = Peripherals {
+            //iomuxc: iomuxc::IOMUXC::new(p.IOMUXC),
+            //systick: cp.SYST,
+            ccm: ccm::CCM::new(ral::ccm::CCM::take()?, ral::ccm_analog::CCM_ANALOG::take()?),
+            //pit: pit::UnclockedPIT::new(p.PIT),
+            dcdc: dcdc::DCDC(ral::dcdc::DCDC::take()?),
+            //pwm1: pwm::UnclockedController::new(),
+            //pwm2: pwm::UnclockedController::new(),
+            //pwm3: pwm::UnclockedController::new(),
+            //pwm4: pwm::UnclockedController::new(),
+            //i2c: i2c::Unclocked::new(),
+            //uart: uart::Unclocked {
+            //uart1: p.LPUART1,
+            //uart2: p.LPUART2,
+            //uart3: p.LPUART3,
+            //uart4: p.LPUART4,
+            //uart5: p.LPUART5,
+            //uart6: p.LPUART6,
+            //uart7: p.LPUART7,
+            //uart8: p.LPUART8,
+        };
+        Some(p)
+    }
+}
