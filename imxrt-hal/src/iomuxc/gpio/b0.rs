18528417402062472aced110babf312b4b672a5e
Ian McIntyre <ianpmcintyre@gmail.com>
2020-03-01 07:46:48 -0500

    Remove GPIO feature-flag

--- imxrt-hal/src/iomuxc/gpio/b0.rs
+++ imxrt-hal/src/iomuxc/gpio/b0.rs
@@ -8,8 +8,6 @@ pad!(
     SW_MUX_CTL_PAD_GPIO_B0_01,
     [alt0, alt1, alt2, alt3, alt4, alt5, alt6, alt8]
 );
-
-#[cfg(feature = "imxrt1062")]
 pad!(
     GPIO_B0_03,
     SW_MUX_CTL_PAD_GPIO_B0_03,
