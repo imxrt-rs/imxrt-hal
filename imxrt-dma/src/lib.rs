d2d8ee8f92a5e91e10c9e444adf5373cb23d621a
Ian McIntyre <ianpmcintyre@gmail.com>
2020-10-18 16:08:33 -0400

    dma: add top-level example

--- imxrt-dma/src/lib.rs
+++ imxrt-dma/src/lib.rs
@@ -1,4 +1,51 @@
 //! Direct Memory Access (DMA) driver for i.MX RT processors
+//!
+//! `imxrt-dma` is a lower-level DMA driver for all i.MX RT processors
+//! It provides an `unsafe` interface for allocating DMA channels, and for
+//! scheduling DMA transactions. `imxrt-dma` also provides some traits and
+//! abscractions that provides simple transfer mechanisms.
+//!
+//! This DMA driver may be re-exported from a HAL. If it is, you should consider
+//! using the safer APIs provided by your HAL, instead of this lower-level API.
+//!
+//! # Example
+//!
+//! Use DMA channel 7 to perform a DMA-powered memory copy.
+//!
+//! ```no_run
+//! use imxrt_dma::{Channel, Transfer};
+//!
+//! let mut channel = unsafe { Channel::new(7) };
+//! channel.reset();
+//!
+//! let source: [u32; 32] = [5; 32];
+//! let destination: [u32; 32] = [0; 32];
+//!
+//! let tx = Transfer::buffer_linear(source.as_ptr(), source.len());
+//! let rx = Transfer::buffer_linear(destination.as_ptr(), destination.len());
+//!
+//! channel.set_always_on();
+//! channel.set_disable_on_completion(true);
+//!
+//! unsafe {
+//!     channel.set_source_transfer(&tx);
+//!     channel.set_destination_transfer(&rx);
+//! }
+//!
+//! channel.set_minor_loop_elements::<u32>(1);
+//! channel.set_transfer_iterations(source.len() as u16);
+//!
+//! channel.set_enable(true);
+//! channel.start();
+//!
+//! if channel.is_error() {
+//!     panic!("Transaction failed!");
+//! }
+//!
+//! while !channel.is_complete() {}
+//!
+//! assert_eq!(destination, [5;32]);
+//! ```
 
 #![no_std]
 
