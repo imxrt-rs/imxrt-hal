0bf8f92dd2e8ee71954586108f68605599faa744
Ian McIntyre <ianpmcintyre@gmail.com>
2020-12-14 20:53:07 -0500

    Reimplement and test the ticks function

--- imxrt1060-hal/src/ccm.rs
+++ imxrt1060-hal/src/ccm.rs
@@ -40,3 +40,37 @@ pub use imxrt_ccm::{
     ral::{I2CClock, PerClock, SPIClock, UARTClock, CCM},
     ClockGate, Handle,
 };
+
+/// Compute the number of clock ticks that represent the microsecond delay
+pub(crate) fn ticks(delay_us: u32, clock_hz: u32) -> u32 {
+    1_000_000_000u32
+        .checked_div(clock_hz)
+        .and_then(|period_ns| {
+            let delay_ns = delay_us.saturating_mul(1_000);
+            delay_ns
+                .checked_div(period_ns)
+                .map(|ticks| ticks.saturating_sub(1))
+        })
+        .unwrap_or(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ticks;
+
+    #[test]
+    fn ticks_zero_clock() {
+        assert_eq!(ticks(1, 0), 0);
+    }
+
+    #[test]
+    fn ticks_gpt() {
+        const HZ: u32 = 8_000_000; // 8MHz
+        assert_eq!(ticks(0, HZ), 0);
+        assert_eq!(ticks(1, HZ), 7);
+        assert_eq!(ticks(1_000, HZ), 7_999);
+        assert_eq!(ticks(1_000_000, HZ), 7_999_999);
+        // Saturating mul
+        assert_eq!(ticks(1_000_000_000, HZ), ticks(0xFFFF_FFFF, HZ));
+    }
+}
