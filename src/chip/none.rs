22be14e018d732b646f1f619a948b4ff116531ab
Ian McIntyre <ianpmcintyre@gmail.com>
2022-12-20 17:04:16 -0500

    Add the "none" chip
    When the user builds without an imxrt-hal feature, we spoof items that
    would have been exported by a chip module. This is that spoofing. The
    top-level module depends on these symbols, so this is to satisfy a
    build.

---
+++ src/chip/none.rs
@@ -0,0 +1,10 @@
+//! The chip API when no chip is selected.
+
+pub mod ccm {
+    pub use crate::common::ccm::*;
+}
+pub mod dma {}
+pub mod lpi2c {}
+
+pub(crate) mod reexports {}
+pub(crate) mod iomuxc {}
