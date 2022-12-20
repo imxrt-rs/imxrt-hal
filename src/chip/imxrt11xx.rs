2d1d975d8c65ba0bb855fb372da4ac2eb5f997f7
Ian McIntyre <ianpmcintyre@gmail.com>
2022-12-20 17:03:33 -0500

    Add 11xx chip configurations
    Similar to the 10xx counterparts, but simpler, since there's less
    11xx-specific content in the HAL.

---
+++ src/chip/imxrt11xx.rs
@@ -0,0 +1,32 @@
+//! Modules shared across all i.MX RT 11xx processors.
+//!
+//! Shared modules may rely on configurations from the `config` module.
+
+pub mod ccm;
+#[path = "dma.rs"]
+pub mod dma;
+pub mod usbphy;
+
+cfg_if::cfg_if! {
+    if #[cfg(chip = "imxrt1170")] {
+        #[path = "imxrt11xx/imxrt1170.rs"]
+        pub(crate) mod config;
+    }
+}
+
+pub(crate) mod reexports {
+    pub use super::usbphy;
+}
+
+pub(crate) mod iomuxc {
+    pub use super::config::pads;
+    use crate::ral;
+
+    /// Transform the `imxrt-ral` IOMUXC instances into pad objects.
+    pub fn into_pads(_: ral::iomuxc::IOMUXC, _: ral::iomuxc_lpsr::IOMUXC_LPSR) -> pads::Pads {
+        // Safety: acquiring pads has the same safety implications
+        // as acquiring the IOMUXC instances. The user has already
+        // assumed the unsafety.
+        unsafe { pads::Pads::new() }
+    }
+}
