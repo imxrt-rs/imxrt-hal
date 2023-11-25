a74fc6984ac4c9cd566af24a3c2db63f4f462dde
Finomnis <finomnis@gmail.com>
2023-11-25 12:23:47 +0100

    Add word_types

--- src/common/lpspi/word_types.rs
+++ src/common/lpspi/word_types.rs
@@ -1,142 +1,171 @@
 use super::LpspiError;
 
-// TODO: need two traits, one for &[X] and one for &mut[x].
-pub trait LpspiInputData {
+/// A data type that can be used for LPSPI transfers
+pub trait LpspiDataBuffer {
     /// Length in bytes
     fn length(&self) -> usize;
 
-    /// Retreive the next chunk to transmit.
+    /// Retreive a chunk to transmit.
     /// Filled with zeros when overflown.
-    fn take(&mut self) -> u32;
-
-    /// Store the next received chunk.
     ///
-    fn store(&mut self, val: u32);
+    /// # Arguments
+    ///
+    /// * `pos` - The `[u32]`-offset from where to read.
+    ///
+    fn read(&self, pos: usize) -> u32;
+
+    /// Store a next received chunk.
+    ///
+    /// # Arguments
+    ///
+    /// * `pos` - The `[u32]`-offset where to write.
+    /// * `val` - The data to write
+    ///
+    fn write(&mut self, pos: usize, val: u32);
 }
 
-pub trait AsLpspiInputData {
-    fn process(
-        &self,
-        f: &dyn FnOnce(&mut dyn LpspiInputData) -> Result<(), LpspiError>,
-    ) -> Result<(), LpspiError>;
-}
-
-struct U8LpspiInputData<'a> {
-    read_pos: usize,
-    write_pos: usize,
-    data: &'a [u8],
-}
-struct U16LpspiInputData<'a> {
-    read_pos: usize,
-    write_pos: usize,
-    data: &'a [u16],
-}
-struct U32LpspiInputData<'a> {
-    read_pos: usize,
-    write_pos: usize,
-    data: &'a [u32],
-}
-
-impl AsLpspiInputData for &[u8] {
-    fn process(
-        &self,
-        f: &dyn FnOnce(&mut dyn LpspiInputData) -> Result<(), LpspiError>,
-    ) -> Result<(), LpspiError> {
-        let mut data = U8LpspiInputData {
-            read_pos: 0,
-            write_pos: 0,
-            data: &self,
-        };
-
-        f(&mut data)
-    }
-}
-impl AsLpspiInputData for &[u16] {
-    fn process(
-        &self,
-        f: &dyn FnOnce(&mut dyn LpspiInputData) -> Result<(), LpspiError>,
-    ) -> Result<(), LpspiError> {
-        let mut data = U16LpspiInputData {
-            read_pos: 0,
-            write_pos: 0,
-            data: &self,
-        };
-
-        f(&mut data)
-    }
-}
-impl AsLpspiInputData for &[u32] {
-    fn process(
-        &self,
-        f: &dyn FnOnce(&mut dyn LpspiInputData) -> Result<(), LpspiError>,
-    ) -> Result<(), LpspiError> {
-        let mut data = U32LpspiInputData {
-            read_pos: 0,
-            write_pos: 0,
-            data: &self,
-        };
-
-        f(&mut data)
-    }
-}
-
-impl LpspiInputData for U8LpspiInputData<'_> {
+impl LpspiDataBuffer for [u8] {
     fn length(&self) -> usize {
-        self.data.len()
+        self.len()
     }
 
-    fn take(&mut self) -> Option<u32> {
-        let mut output = 0;
+    fn read(&self, pos: usize) -> u32 {
+        let pos = 4 * pos;
 
-        for _ in 0..4 {
-            output = output << 8;
-            output += u32::from(*self.data.get(self.read_pos)?);
-            self.read_pos += 1;
+        u32::from_be_bytes([
+            self.get(pos + 0).copied().unwrap_or_default(),
+            self.get(pos + 1).copied().unwrap_or_default(),
+            self.get(pos + 2).copied().unwrap_or_default(),
+            self.get(pos + 3).copied().unwrap_or_default(),
+        ])
+    }
+
+    fn write(&mut self, pos: usize, val: u32) {
+        let pos = 4 * pos;
+
+        for (offset, val) in val.to_be_bytes().into_iter().enumerate() {
+            if let Some(x) = self.get_mut(pos + offset) {
+                *x = val;
+            }
         }
-
-        Some(output)
-    }
-
-    fn store(&mut self, val: u32) {
-        todo!()
     }
 }
 
-impl LpspiInputData for U16LpspiInputData<'_> {
+impl LpspiDataBuffer for [u16] {
     fn length(&self) -> usize {
-        self.data.len() * 2
+        self.len() * 2
     }
 
-    fn take(&mut self) -> u32 {
-        // Only check the first one, because we need to zero fill otherwise
-        let mut output = 0;
+    fn read(&self, pos: usize) -> u32 {
+        let pos = 2 * pos;
 
-        for _ in 0..2 {
-            output = output << 16;
-            output += u32::from(self.data.get(self.read_pos).copied().unwrap_or_default());
-            self.read_pos += 1;
+        let [b0, b1] = self.get(pos + 0).copied().unwrap_or_default().to_be_bytes();
+        let [b2, b3] = self.get(pos + 1).copied().unwrap_or_default().to_be_bytes();
+
+        u32::from_be_bytes([b0, b1, b2, b3])
+    }
+
+    fn write(&mut self, pos: usize, val: u32) {
+        let pos = 2 * pos;
+
+        let [b0, b1, b2, b3] = val.to_be_bytes();
+
+        if let Some(x) = self.get_mut(pos + 0) {
+            *x = u16::from_be_bytes([b0, b1]);
         }
-
-        output
-    }
-
-    fn store(&mut self, val: u32) {
-        todo!()
+        if let Some(x) = self.get_mut(pos + 1) {
+            *x = u16::from_be_bytes([b2, b3]);
+        }
     }
 }
 
-impl LpspiInputData for U32LpspiInputData<'_> {
+impl LpspiDataBuffer for [u32] {
     fn length(&self) -> usize {
-        self.data.len() * 4
+        self.len() * 4
     }
 
-    fn take(&mut self) -> Option<u32> {
-        let data = *self.data.get(self.read_pos)?;
-        self.read_pos += 1;
-        Some(data)
+    fn read(&self, pos: usize) -> u32 {
+        self.get(pos).copied().unwrap_or_default()
     }
 
-    fn store(&mut self, val: u32) {
-        todo!()
+    fn write(&mut self, pos: usize, val: u32) {
+        if let Some(x) = self.get_mut(pos) {
+            *x = val;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! check_read {
+        ($t:ty, $data:expr, $pos:expr, $expected:expr) => {{
+            let buf: &[$t] = &$data;
+            assert_eq!(buf.read($pos), $expected)
+        }};
+    }
+
+    macro_rules! check_write {
+        ($init_val:expr, $pos:expr, $val:expr, $expected:expr) => {{
+            let buf = &mut $init_val;
+            buf.write($pos, $val);
+            assert_eq!(buf, &$expected)
+        }};
+    }
+
+    #[test]
+    fn read_u8() {
+        check_read!(
+            u8,
+            [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde],
+            0,
+            0x12345678
+        );
+        check_read!(
+            u8,
+            [0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde],
+            1,
+            0x9abcde00
+        );
+    }
+
+    #[test]
+    fn write_u8() {
+        check_write!([0u8; 5], 0, 0x12345678, [0x12, 0x34, 0x56, 0x78, 0]);
+        check_write!(
+            [0u8; 9],
+            1,
+            0x12345678,
+            [0, 0, 0, 0, 0x12, 0x34, 0x56, 0x78, 0]
+        );
+        check_write!([0u8; 5], 1, 0x12345678, [0, 0, 0, 0, 0x12]);
+    }
+
+    #[test]
+    fn read_u16() {
+        check_read!(u16, [0x1234, 0x5678, 0x9abc], 0, 0x12345678);
+        check_read!(u16, [0x1234, 0x5678, 0x9abc], 1, 0x9abc0000);
+    }
+
+    #[test]
+    fn write_u16() {
+        check_write!([0u16; 3], 0, 0x12345678, [0x1234, 0x5678, 0]);
+        check_write!([0u16; 5], 1, 0x12345678, [0, 0, 0x1234, 0x5678, 0]);
+        check_write!([0u16; 3], 1, 0x12345678, [0, 0, 0x1234]);
+    }
+
+    #[test]
+    fn read_u32() {
+        check_read!(u32, [0x12345678, 0x9abcdeff], 0, 0x12345678);
+        check_read!(u32, [0x12345678, 0x9abcdeff], 1, 0x9abcdeff);
+        check_read!(u32, [0x12345678, 0x9abcdeff], 2, 0);
+    }
+
+    #[test]
+    fn write_u32() {
+        check_write!([0u32; 2], 0, 0x12345678, [0x12345678, 0]);
+        check_write!([0u32; 2], 1, 0x12345678, [0, 0x12345678]);
+        check_write!([0u32; 2], 2, 0x12345678, [0, 0]);
     }
 }
