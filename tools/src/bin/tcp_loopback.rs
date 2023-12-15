02df7e35bd3a1c27d230f7f04d4f6da53c2540af
Ian McIntyre <ianpmcintyre@gmail.com>
2023-12-15 12:23:06 -0500

    Extend ENET demo to include UDP broadcast
    Use the constant enum to change the example's behavior. This commit also
    allows the TCP socket to be re-used after the client disconnects. Each
    example works in debug and release builds (though I needed a separate
    fix in the ENET driver due to a poorly-written debug assert).

--- tools/src/bin/tcp_loopback.rs
+++ tools/src/bin/tcp_loopback.rs
@@ -10,15 +10,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ip: std::net::Ipv4Addr = ip.parse()?;
 
     let mut client = TcpStream::connect(std::net::SocketAddrV4::new(ip, 5000))?;
+    client.set_nodelay(true)?;
 
     const MSG: &[u8] = b"Hello, world!";
     client.write(MSG)?;
     let mut resp = [0; MSG.len()];
     client.read(&mut resp)?;
 
-    if resp == MSG {
-        Ok(())
-    } else {
-        Err(format!("Expected '{MSG:?}' but received '{resp:?}'").into())
+    if resp != MSG {
+        return Err(format!("Expected '{MSG:?}' but received '{resp:?}'").into());
     }
+
+    let mut resp = vec![0; 256];
+    for idx in 0..10 {
+        let msg: Vec<u8> = Vec::from_iter((0..=255u8).cycle().skip(idx).take(resp.len()));
+        client.write(&msg)?;
+        client.read(&mut resp)?;
+        if msg != resp {
+            return Err(format!("Mismatched buffer on {idx} attempt").into());
+        }
+    }
+
+    Ok(())
 }
