659915206ce9f7dcee7c1fec814ea44003811404
Trevin Price <113460795+Trevin-Price@users.noreply.github.com>
2024-08-04 13:48:10 -0400

    Revert "Added PLL7."
    This reverts commit a18cda8712700d37087cc3eb69b6af443fb326e1.

--- src/chip/imxrt10xx/ccm/analog.rs
+++ src/chip/imxrt10xx/ccm/analog.rs
@@ -20,9 +20,10 @@ pub mod pll2 {
     pub const MAX_FRAC: u8 = super::pll3::MAX_FRAC;
 }
 
-/// The USB1 PLL.
+/// The USB PLL.
 ///
-/// This PLL is associated with USB1, the primary USB interface.
+/// When an implementation has multiple USB peripherals, this
+/// PLL is associated with USB1.
 pub mod pll3 {
     /// PLL3 frequency (Hz).
     ///
@@ -38,7 +39,7 @@ pub mod pll3 {
 
     use crate::ral;
 
-    /// Restart the USB1 PLL.
+    /// Restart the USB(1) PLL.
     pub fn restart(ccm_analog: &mut ral::ccm_analog::CCM_ANALOG) {
         loop {
             if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, ENABLE == 0) {
@@ -58,51 +59,6 @@ pub mod pll3 {
             }
             if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB1, EN_USB_CLKS == 0) {
                 ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB1_SET, EN_USB_CLKS: 1);
-                continue;
-            }
-            break;
-        }
-    }
-}
-
-/// The USB2 PLL.
-///
-/// This PLL is associated with USB2, the secondary USB interface.
-pub mod pll7 {
-    /// PLL7 frequency (Hz).
-    ///
-    /// The reference manual notes that PLL7 should always run at 480MHz,
-    /// so this constant assumes that PLL7's DIV_SELECT field isn't
-    /// changed at runtime.
-    pub const FREQUENCY: u32 = 480_000_000;
-
-    /// The smallest PLL7_PFD divider.
-    pub const MIN_FRAC: u8 = 12;
-    /// The largest PLL7_PFD divider.
-    pub const MAX_FRAC: u8 = 35;
-
-    use crate::ral;
-
-    /// Restart the USB2 PLL.
-    pub fn restart(ccm_analog: &mut ral::ccm_analog::CCM_ANALOG) {
-        loop {
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB2, ENABLE == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB2_SET, ENABLE: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB2, POWER == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB2_SET, POWER: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB2, LOCK == 0) {
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB2, BYPASS == 1) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB2_CLR, BYPASS: 1);
-                continue;
-            }
-            if ral::read_reg!(ral::ccm_analog, ccm_analog, PLL_USB2, EN_USB_CLKS == 0) {
-                ral::write_reg!(ral::ccm_analog, ccm_analog, PLL_USB2_SET, EN_USB_CLKS: 1);
                 continue;
             }
             break;
