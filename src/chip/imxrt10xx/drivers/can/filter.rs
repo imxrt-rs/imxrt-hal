11a129cebac4a064f320028336ade17b6c8a2181
sigil-03 <sigil@glyphs.tech>
2024-08-20 12:29:31 -0600

    add filter doc strings

--- src/chip/imxrt10xx/drivers/can/filter.rs
+++ src/chip/imxrt10xx/drivers/can/filter.rs
@@ -1,27 +1,47 @@
 //! Filter bank API.
 
+/// Decodes value of the ID Extended (IDE) and Remote Transmission Request (RTR) bits
+///
+/// TODO: Break this up into seperate RTR and IDE structs
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub enum FlexCanIde {
+    /// IDE = 0, RTR = 0
     #[default]
     None = 0,
+    /// IDE = 1, RTR = 0
     Ext = 1,
+    /// IDE = 0, RTR = 1
     Rtr = 2,
+    /// IDE = 1, RTR = 1
     Std = 3,
+    /// Unused(?) variant
     Inactive,
 }
 
+/// Filter enable/disable.
+///
+/// Used by [`set_fifo_filter_mask()`]
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub enum FlexCanFlten {
+    /// Accept all CAN frames (filtering is disabled)
     AcceptAll = 0,
+    /// Reject all CAN frames that do not match the filter exactly
     #[default]
     RejectAll = 1,
 }
 
+/// FlexCAN Filter.
+///
+/// Use this struct in conjunction with [`set_fifo_filter()`]
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Default)]
 pub struct FlexCanFilter {
+    /// Mailbox ID that this filter will apply to
     pub filter_id: u8,
+    /// Frame ID
     pub id: u32,
+    /// Extended ID Bit (IDE)
     pub ide: FlexCanIde,
+    /// Remote Frame Bit (RTR)
     pub remote: FlexCanIde,
 }
 
