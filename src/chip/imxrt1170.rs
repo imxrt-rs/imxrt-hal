d8d3d8e03e4b0940aa608880ccf7706878fbdb9c
Ian McIntyre <me@mciantyre.dev>
2026-02-28 16:53:12 -0500

    Bring forward dependencies, examples

--- src/chip/imxrt1170.rs
+++ src/chip/imxrt1170.rs
@@ -17,8 +17,10 @@ pub(crate) mod iomuxc {
 
 mod drivers {
     pub mod dma;
+    pub mod gpc;
     pub mod gpio;
     pub mod pit;
+    pub mod pmu;
     pub mod sai;
     pub mod snvs;
     pub mod timer;
